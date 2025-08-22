aa1273a4fa982551227867dd9f65390f312e8621
fuxi <1538130391@qq.com>
2025-08-22 20:22:04 +0800

    2

---
+++ cpp/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,47 @@
+﻿#include <vector>
+
+class Solution
+{
+public:
+	std::vector<std::vector<int>> generateMatrix(int n)
+	{
+		std::vector<std::vector<int>> res(n, std::vector<int>(n));
+		int i = -1, j = -1, count = n, num = 0;
+		while (true)
+		{
+			if (count == 0)
+			{
+				break;
+			}
+			
+			i++;
+			j++;
+			
+			if (count == 1)
+			{
+				res[i][j] = ++num;
+				break;
+			}
+			
+			for (int k = 0; k < count - 1; k++)
+			{
+				res[i][j++] = ++num;
+			}
+			for (int k = 0; k < count - 1; k++)
+			{
+				res[i++][j] = ++num;
+			}
+			for (int k = 0; k < count - 1; k++)
+			{
+				res[i][j--] = ++num;
+			}
+			for (int k = 0; k < count - 1; k++)
+			{
+				res[i--][j] = ++num;
+			}
+
+			count -= 2;
+		}
+		return res;
+	}
+};
---
+++ cpp/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+
+class Solution
+{
+public:
+	int minSubArrayLen(int target, std::vector<int>& nums)
+	{
+		int size = nums.size();
+		std::vector<std::vector<int>> dp(size, std::vector<int>(size));
+		int min_length = INT_MAX;
+		for (int i = 0; i < size; i++)
+		{
+			dp[i][i] = nums[i];
+		}
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (j > i)
+				{
+					dp[i][j] = dp[i][j-1] + dp[j][j];
+				}
+				if (dp[i][j] >= target)
+				{
+					auto length = j - i + 1;
+					min_length = min_length < length ? min_length : length;
+				}
+			}
+		}
+
+		return min_length;
+	}
+};
