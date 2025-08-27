6bbca6d9564b6ab42b4cfbcdba2a05148c65369c
fuxi <1538130391@qq.com>
2025-08-27 20:22:38 +0800

    2

---
+++ cpp/HashTable/0242-Valid-Anagram.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+
+class Solution
+{
+public:
+	// 击败100%，还可以用排序
+	bool isAnagram(std::string s, std::string t)
+	{
+		if (s.length() != t.length())
+		{
+			return false;
+		}
+		std::vector<int> table(26,0);
+		int count = 0;
+		for (char c : s)
+		{
+			int index = c - 'a';
+			if (table[index] == 0)
+				count++;
+			table[index]++;
+		}
+		for (auto c : t)
+		{
+			int index = c - 'a';
+			table[index]--;
+			if (table[index] == 0)
+				count--;
+		}
+		if (count == 0)
+			return true;
+		return false;
+	}
+};
---
+++ cpp/HashTable/0349-Intersection-of-Two-Arrays.cpp
@@ -0,0 +1,25 @@
+﻿#include <unordered_set>
+#include <vector>
+
+class Solution
+{
+public:
+	std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2)
+	{
+		std::unordered_set<int> set{}, res{};
+		for (int n : nums1)
+			set.insert(n);
+		for (int n : nums2)
+		{
+			if (set.find(n) != set.end())
+				res.insert(n);
+			
+		}
+
+		std::vector<int> r{};
+		r.reserve(res.size());
+		for (int n : res)
+			r.emplace_back(n);
+		return r;
+	}
+};
