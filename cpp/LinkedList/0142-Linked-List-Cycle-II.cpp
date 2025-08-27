8f8cc49d83bd0c07c6f762ec55fa17595f6910f9
fuxi <1538130391@qq.com>
2025-08-27 19:23:26 +0800

    1

---
+++ cpp/LinkedList/0142-Linked-List-Cycle-II.cpp
@@ -0,0 +1,55 @@
+﻿struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+#include <unordered_set>
+
+class Solution
+{
+public:
+	// 哈希表，时间少，空间多
+	// ListNode* detectCycle(ListNode* head)
+	// {
+	// 	std::unordered_set<ListNode*> set{};
+	// 	ListNode* node = head;
+	// 	while (node != nullptr)
+	// 	{
+	// 		if (set.find(node) != set.end())
+	// 		{
+	// 			return node;
+	// 		}
+	// 		set.insert(node);
+	// 		node = node->next;
+	// 	}
+	// 	return nullptr;
+	// }
+
+	// 快慢指针，数学计算可得，快慢指针相遇时，再来个从头开始的 ptr，它与 slow 距离环入口相等
+	ListNode* detectCycle(ListNode* head)
+	{
+		ListNode *slow = head, *fast = head;
+		while (fast != nullptr)
+		{
+			slow = slow->next;
+			if (fast->next == nullptr)
+			{
+				return nullptr;
+			}
+			fast = fast->next->next;
+			if (fast == slow)
+			{
+				ListNode* ptr = head;
+				while (ptr != slow)
+				{
+					ptr = ptr->next;
+					slow = slow->next;
+				}
+				return ptr;
+			}
+		}
+		return nullptr;
+	}
+};
