84df195ba3bfc734c5bda4523571f3f7331aac1d
fuxi <1538130391@qq.com>
2025-08-27 19:23:19 +0800

    1

---
+++ cpp/LinkedList/02.07.Intersection-of-Two-Linked-Lists.cpp
@@ -0,0 +1,62 @@
+﻿#include <unordered_set>
+#include <vector>
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution
+{
+public:
+	/* vector用时超5%，内存超8%
+	 * unordered_set用时超23%，内存超6%
+	 */
+	// ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+	// {
+	// 	std::unordered_set<ListNode *> visited;
+	// 	ListNode *temp = headA;
+	// 	while (temp != nullptr) {
+	// 		visited.insert(temp);
+	// 		temp = temp->next;
+	// 	}
+	// 	temp = headB;
+	// 	while (temp != nullptr) {
+	// 		if (visited.count(temp)) {
+	// 			return temp;
+	// 		}
+	// 		temp = temp->next;
+	// 	}
+	// 	return nullptr;
+	// }
+	// ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+	// {
+	// 	if (headA == nullptr || headB == nullptr)
+	// 	{
+	// 		return nullptr;
+	// 	}
+	// 	ListNode *pA = headA, *pB = headB;
+	// 	while (pA != pB)
+	// 	{
+	// 		pA = pA == nullptr ? headB : pA->next;
+	// 		pB = pB == nullptr ? headA : pB->next;
+	// 	}
+	// 	return pA;
+	// }
+	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+	{
+		if (headA == nullptr || headB == nullptr)
+		{
+			return nullptr;
+		}
+		ListNode *a = headA, *b = headB;
+		while (a != b)
+		{
+			a = a == nullptr ? headB : a->next;
+			b = b == nullptr ? headA : b->next;
+		}
+		return a;
+	}
+};
