3dc2ec47c526a92a1e59d5f744ad7fe8c97d4d99
fuxi <1538130391@qq.com>
2025-08-28 20:43:08 +0800

    2

---
+++ cpp/HashTable/0001-Two-Sum.cpp
@@ -0,0 +1,22 @@
+﻿#include <unordered_map>
+#include <vector>
+
+class Solution
+{
+public:
+	std::vector<int> twoSum(std::vector<int>& nums, int target)
+	{
+		std::unordered_map<int, int> map{};
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int sup = target - nums[i];
+			if (map.find(sup) != map.end())
+				return {i, map[sup]};
+
+			map.insert({nums[i], i});
+		}
+
+		return {};
+	}
+};
---
+++ cpp/HashTable/0202-Happy-Number.cpp
@@ -0,0 +1,28 @@
+﻿#include <unordered_set>
+
+class Solution
+{
+public:
+	bool isHappy(int n)
+	{
+		std::unordered_set<int> set{};
+		while (n != 1)
+		{
+			// 出现循环，不是快乐数
+			if (set.find(n) != set.end())
+				return false;
+			set.insert(n);
+			
+			int sum = 0;
+			while (n > 0)
+			{
+				int a = n % 10;
+				n = n / 10;
+				sum += a * a;
+			}
+
+			n = sum;
+		}
+		return true;
+	}
+};
