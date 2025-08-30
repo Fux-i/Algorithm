53c7258f85ec0fd904f93902923178ffaa040dfb
fuxi <1538130391@qq.com>
2025-08-30 19:53:47 +0800

    1

---
+++ cpp/HashTable/0454-4sum-ii.cpp
@@ -0,0 +1,21 @@
+﻿#include <unordered_map>
+#include <vector>
+
+class Solution
+{
+public:
+	int fourSumCount(std::vector<int>& nums1, std::vector<int>& nums2, std::vector<int>& nums3,
+	                 std::vector<int>& nums4)
+	{
+		std::unordered_map<int, int> countAB;
+		for (int u : nums1)
+			for (int v : nums2)
+				++countAB[u + v];
+		int ans = 0;
+		for (int u : nums3)
+			for (int v : nums4)
+				if (countAB.find(-u - v) != countAB.end())
+					ans += countAB[-u - v];
+		return ans;
+	}
+};
