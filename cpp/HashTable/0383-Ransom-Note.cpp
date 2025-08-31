07cba6e00fd2bc6ecab562343b4482b90c6955a1
fuxi <1538130391@qq.com>
2025-08-31 08:44:54 +0800

    1

---
+++ cpp/HashTable/0383-Ransom-Note.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <unordered_map>
+
+class Solution
+{
+public:
+	bool canConstruct(std::string ransomNote, std::string magazine)
+	{
+		std::unordered_map<char, short> map{};
+		for (char c : magazine)
+			map[c]++;
+		for (char c : ransomNote)
+		{
+			if (map.find(c) == map.end())
+				return false;
+			if (--map[c] == -1)
+				return false;
+		}
+		return true;
+	}
+};
