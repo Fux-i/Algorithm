10f5534b8594521da3f170fda4fd33c7ba92f132
fuxi <1538130391@qq.com>
2025-09-05 09:48:38 +0800

    2

---
+++ cpp/String/Easy-0344-Reverse-String.cpp
@@ -0,0 +1,14 @@
+﻿#include <vector>
+
+class Solution {
+public:
+	void reverseString(std::vector<char>& s) {
+		auto size = s.size();
+		for (size_t i = 0; i < size / 2; i++)
+		{
+			char temp = s[i];
+			s[i] = s[size - 1 - i];
+			s[size - 1 - i] = temp;
+		}
+	}
+};
---
+++ cpp/String/Easy-0541-Reverse-String-II.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+
+class Solution
+{
+public:
+	std::string reverseStr(std::string s, int k)
+	{
+		auto size = s.size();
+		for (int i = 0; i < size; i += (2 * k))
+		{
+			if (i + k > size)
+				std::reverse(s.begin() + i, s.end());
+			else
+				std::reverse(s.begin() + i, s.begin() + i + k);
+		}
+		return s;
+	}
+};
