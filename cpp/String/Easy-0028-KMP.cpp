696263cffb06669a26bb08a689001957adaa4920
fuxi <1538130391@qq.com>
2025-09-08 10:41:25 +0800

    Create Easy-0028-KMP.cpp

---
+++ cpp/String/Easy-0028-KMP.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+
+class Solution
+{
+public:
+	int strStr(std::string txt, std::string pattern)
+	{
+		int n = txt.size(), m = pattern.size();
+		if (m == 0)
+			return 0;
+		std::vector<int> next(m);
+		for (int i = 1, j = 0; i < m; i++)
+		{
+			while (j > 0 && pattern[i] != pattern[j])
+				j = next[j - 1];
+			if (pattern[i] == pattern[j])
+				j++;
+			next[i] = j;
+		}
+		for (int i = 0, j = 0; i < n; i++)
+		{
+			while (j > 0 && txt[i] != pattern[j])
+				j = next[j - 1];
+			if (txt[i] == pattern[j])
+				j++;
+			if (j == m)
+				return i - m + 1;
+		}
+		return -1;
+	}
+};
