6a29827f8dae31c39dcc93adca815749dc1ce651
fuxi <1538130391@qq.com>
2025-09-15 09:35:12 +0800

    2

---
+++ cpp/Stack & Queue/Easy-0225-Implement-Stack-Using-Queues.cpp
@@ -0,0 +1,61 @@
+﻿/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+#include <queue>
+
+class MyStack
+{
+public:
+	MyStack() {}
+
+	void push(int x)
+	{
+		if (pushQ1)
+		{
+			q1.push(x);
+			while (!q2.empty())
+			{
+				q1.push(q2.front());
+				q2.pop();
+			}
+			pushQ1 = !pushQ1;
+		}
+		else
+		{
+			q2.push(x);
+			while (!q1.empty())
+			{
+				q2.push(q1.front());
+				q1.pop();
+			}
+			pushQ1 = !pushQ1;
+		}
+	}
+
+	int pop()
+	{
+		int temp = pushQ1 ? q2.front() : q1.front();
+		pushQ1 ? q2.pop() : q1.pop();
+		return temp;
+	}
+
+	int top()
+	{
+		return pushQ1 ? q2.front() : q1.front();
+	}
+
+	bool empty()
+	{
+		return pushQ1 ? q2.empty() : q1.empty();
+	}
+private:
+	std::queue<int> q1;
+	std::queue<int> q2;
+	bool pushQ1 = true;
+};
