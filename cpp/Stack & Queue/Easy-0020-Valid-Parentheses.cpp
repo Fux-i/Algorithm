c92ce56605ef4fe8a09cef8f8da9a64e8afa1c96
fuxi <1538130391@qq.com>
2025-09-17 09:24:41 +0800

    2

---
+++ cpp/Stack & Queue/Easy-0020-Valid-Parentheses.cpp
@@ -0,0 +1,40 @@
+﻿#include <array>
+#include <stack>
+#include <string>
+
+class Solution
+{
+public:
+	bool isValid(std::string s)
+	{
+		for (char c : s)
+		{
+			if (std::find(l_.begin(), l_.end(), c) != l_.end())
+				s_.push(c);
+			else
+			{
+				if (s_.empty())
+					return false;
+				switch (s_.top())
+				{
+				case '(':
+					if (c != ')')
+						return false;
+					break;
+				case '[':
+					if (c != ']')
+						return false;
+					break;
+				default:
+					if (c != '}')
+						return false;
+				}
+				s_.pop();
+			}
+		}
+		return s_.empty();
+	}
+private:
+	std::stack<char> s_;
+	std::array<char, 3> l_{'(', '[', '{'};
+};
---
+++ cpp/Stack & Queue/Easy-1047-Remove-All-Adjacent-Duplication.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <string>
+
+class Solution {
+public:
+	std::string removeDuplicates(std::string s)
+	{
+		for (char c : s)
+		{
+			if (!v_.empty() && v_.back() == c)
+				v_.pop_back();
+			else
+				v_.push_back(c);
+		}
+
+		for (int i = 0; i < v_.size(); i++)
+			s[i] = v_[i];
+
+		s.resize(v_.size());
+		return s;
+	}
+private:
+	std::vector<char> v_;
+};
