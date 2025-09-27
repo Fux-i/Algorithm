6cebef5b2f0135c1a72f51c0d79de674f68a96e5
fuxi <1538130391@qq.com>
2025-09-27 09:21:56 +0800

    Create Easy-0144-Binary-Tree-Preorder-Traversal.cpp

---
+++ cpp/BinaryTree/Easy-0144-Binary-Tree-Preorder-Traversal.cpp
@@ -0,0 +1,48 @@
+﻿#include <queue>
+#include <stack>
+#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution
+{
+public:
+	std::vector<int> preorderTraversal(TreeNode* root)
+	{
+		std::vector<int> ret;
+		std::stack<TreeNode*> stk;
+		TreeNode* node = root;
+		while (!stk.empty() || node != nullptr)
+		{
+			while (node != nullptr)
+			{
+				ret.emplace_back(node->val);
+				stk.emplace(node);
+				node = node->left;
+			}
+			node = stk.top();
+			stk.pop();
+			node = node->right;
+		}
+
+		return ret;
+	}
+
+	// void rec(TreeNode* node, std::vector<int>& v)
+	// {
+	// 	if (!node)
+	// 		return;
+	// 	v.push_back(node->val);
+	// 	rec(node->left, v);
+	// 	rec(node->right, v);
+	// }
+};
