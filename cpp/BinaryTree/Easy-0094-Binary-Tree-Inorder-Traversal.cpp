a6b278598f3f394b300823b2954d781c8bda5cc0
fuxi <1538130391@qq.com>
2025-09-27 10:12:21 +0800

    2

---
+++ cpp/BinaryTree/Easy-0094-Binary-Tree-Inorder-Traversal.cpp
@@ -0,0 +1,69 @@
+﻿#include <queue>
+#include <stack>
+#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution
+{
+public:
+	std::vector<int> inorderTraversal(TreeNode* root)
+	{
+		std::vector<int> ret;
+		std::stack<TreeNode*> stk;
+		
+		TreeNode* node = root;
+		while (!stk.empty() || node != nullptr)
+		{
+			while (node != nullptr)
+			{
+				stk.emplace(node);
+				node = node->left;
+			}
+			node = stk.top();
+			stk.pop();
+			ret.emplace_back(node->val);
+			node = node->right;
+		}
+
+		return ret;
+	}
+};
+
+/** 空指针标记法
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        if (root != NULL) st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            if (node != NULL) {
+                st.pop(); // 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
+                if (node->right) st.push(node->right);  // 添加右节点（空节点不入栈）
+
+                st.push(node);                          // 添加中节点
+                st.push(NULL); // 中节点访问过，但是还没有处理，加入空节点做为标记。
+
+                if (node->left) st.push(node->left);    // 添加左节点（空节点不入栈）
+            } else { // 只有遇到空节点的时候，才将下一个节点放进结果集
+                st.pop();           // 将空节点弹出
+                node = st.top();    // 重新取出栈中元素
+                st.pop();
+                result.push_back(node->val); // 加入到结果集
+            }
+        }
+        return result;
+    }
+};
+*/
---
+++ cpp/BinaryTree/Easy-0145-Binary-Tree-Postorder-Traversal.cpp
@@ -0,0 +1,45 @@
+﻿#include <queue>
+#include <stack>
+#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution
+{
+public:
+	std::vector<int> postorderTraversal(TreeNode* root)
+	{
+		std::vector<int> ret;
+		std::stack<std::pair<TreeNode*, bool>> stk;	// bool 表示是否标记过
+		
+		stk.emplace(root, false);
+		while (!stk.empty())
+		{
+			auto [node, flag] = stk.top();
+			stk.pop();
+			if (node == nullptr)
+				continue;
+			if (flag == false)
+			{
+				stk.emplace(node, true);
+				stk.emplace(node->right, false);
+				stk.emplace(node->left, false);
+			}
+			else
+			{
+				ret.push_back(node->val);
+			}
+		}
+
+		return ret;
+	}
+};
