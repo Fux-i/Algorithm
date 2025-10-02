64bfba93d18e1d8e1f0ec29eaa0168d846adbd6c
fuxi <1538130391@qq.com>
2025-10-02 09:11:27 +0800

    2

---
+++ cpp/BinaryTree/Easy-0101-Symmetric.cpp
@@ -0,0 +1,31 @@
+﻿struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	bool isSymmetric(TreeNode* root)
+	{
+		return rec(root->left, root->right);
+	}
+
+	bool rec(TreeNode* lhs, TreeNode* rhs)
+	{
+		if (lhs == nullptr || rhs == nullptr)
+		{
+			if (lhs == rhs)
+				return true;
+			else
+				return false;
+		}
+		
+		return lhs->val == rhs->val && rec(lhs->right, rhs->left) && rec(lhs->left, rhs->right);
+	}
+};
