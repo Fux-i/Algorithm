30da7c1d445e1d7267df5fef9c2d9be2667bc3e3
fuxi <1538130391@qq.com>
2025-10-02 10:23:47 +0800

    2

---
+++ cpp/BinaryTree/Easy-0110-balanced-binary-tree.cpp
@@ -0,0 +1,35 @@
+﻿#include <algorithm>
+#include <cstdlib>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	bool isBalanced(TreeNode* root)
+	{
+		return getBalancedDepth(root) != -1;
+	}
+
+	// 返回树的高度，若不是平衡二叉树则返回-1
+	int getBalancedDepth(TreeNode* root)
+	{
+		if (!root)
+			return 0;
+		int ld = getBalancedDepth(root->left);
+		int rd = getBalancedDepth(root->right);
+		if (ld == -1 || rd == -1)
+			return -1;
+		if (abs(ld - rd) > 1)
+			return -1;
+		return 1 + std::max(ld, rd);
+	}
+};
---
+++ cpp/BinaryTree/Easy-0257-binary-tree-paths.cpp
@@ -0,0 +1,45 @@
+﻿#include <algorithm>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+	std::vector<std::string> ret;
+public:
+	std::vector<std::string> binaryTreePaths(TreeNode* root)
+	{
+		rec(root, "");
+		return ret;
+	}
+
+	void rec(TreeNode* node, std::string path)
+	{
+		if (path.empty())
+			path.append(std::to_string(node->val));
+		else
+		{
+			path.append(std::string("->"));
+			path.append(std::to_string(node->val));
+		}
+		if (!node->left && !node->right)
+			ret.push_back(path);
+		else
+		{
+			if (node->left)
+				rec(node->left, path);
+			if (node->right)
+				rec(node->right, path);
+		}
+	}
+};
