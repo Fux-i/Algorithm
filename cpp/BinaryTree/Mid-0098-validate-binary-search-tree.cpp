bfbff85f965582134ea23d091ca0982ff304fe26
fuxi <1538130391@qq.com>
2025-10-04 09:50:34 +0800

    3

---
+++ cpp/BinaryTree/Mid-0098-validate-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdint.h>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+	int lastVal = INT32_MIN;
+	bool isBST = true;
+	bool isFirstTime = true;
+public:
+	bool isValidBST(TreeNode* root)
+	{
+		dfs(root);
+		return isBST;
+	}
+
+	void dfs(TreeNode* node)
+	{
+		if (!node)
+			return;
+		dfs(node->left);
+		if (isFirstTime)
+			isFirstTime = false;
+		else if (node->val <= lastVal)
+		{
+			isBST = false;
+			return;
+		}
+		lastVal = node->val;
+		dfs(node->right);
+	}
+};
---
+++ cpp/BinaryTree/Mid-0654-maximum-binary-tree.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	TreeNode* constructMaximumBinaryTree(std::vector<int>& nums)
+	{
+		using vec = std::vector<int>;
+		auto it = std::max_element(nums.begin(), nums.end());
+		auto node = new TreeNode(*it);
+		if (nums.begin() < it)
+		{
+			vec leftNums(nums.begin(), it);
+			node->left = constructMaximumBinaryTree(leftNums);
+		}
+		if (it < nums.end() - 1)
+		{
+			vec rightNums(it + 1, nums.end());
+			node->right = constructMaximumBinaryTree(rightNums);
+		}
+		return node;
+	}
+};
