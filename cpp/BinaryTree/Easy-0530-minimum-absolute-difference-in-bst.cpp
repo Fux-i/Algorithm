9c5375e3ff65bda8c6c2ec7b26afdb2ac276ecdb
fuxi <1538130391@qq.com>
2025-10-05 09:52:24 +0800

    2

---
+++ cpp/BinaryTree/Easy-0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,38 @@
+﻿#include <algorithm>
+#include <stdint.h>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+	int lastVal = 0;
+	int minDiff = INT32_MAX;
+	bool isFirstTime = true;
+public:
+	int getMinimumDifference(TreeNode* root)
+	{
+		dfs(root);
+		return minDiff;
+	}
+
+	void dfs(TreeNode* node)
+	{
+		if (!node)
+			return;
+		dfs(node->left);
+		if (isFirstTime)
+			isFirstTime = false;
+		else
+			minDiff = std::min(minDiff, node->val - lastVal);
+		lastVal = node->val;
+		dfs(node->right);
+	}
+};
---
+++ cpp/BinaryTree/Mid-0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,84 @@
+﻿#include <queue>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	TreeNode* ans;
+
+	bool dfs(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+		if (root == nullptr)
+			return false;
+		bool lson = dfs(root->left, p, q);	// 左子树是否包含p或q
+		bool rson = dfs(root->right, p, q);	// 右子树是否包含p或q
+		if ((lson && rson) || ((root->val == p->val || root->val == q->val) && (lson || rson)))
+			ans = root;
+		return lson || rson || (root->val == p->val || root->val == q->val);
+	}
+
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+		dfs(root, p, q);
+		return ans;
+	}
+};
+
+/* 5%, 5%
+class Solution
+{
+	TreeNode* lastAncestor = nullptr;
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+		lastAncestor = root;
+		if (root->left)
+			dfs(root->left, p, q);
+		if (root->right)
+			dfs(root->right, p, q);
+		return lastAncestor;
+	}
+
+	void dfs(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+		if (!isAncestor(root, p, q))
+			return;
+
+		lastAncestor = root;
+		if (root->left)
+			dfs(root->left, p, q);
+		if (root->right)
+			dfs(root->right, p, q);
+	}
+
+	bool isAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+		int found = 0;
+		std::queue<TreeNode*> qu;
+		qu.push(root);
+		while (!qu.empty())
+		{
+			auto node = qu.front();
+			qu.pop();
+			if (node == p || node == q)
+				found++;
+			if (node->left)
+				qu.push(node->left);
+			if (node->right)
+				qu.push(node->right);
+		}
+		if (found == 2)
+			return true;
+		return false;
+	}
+};
+*/
