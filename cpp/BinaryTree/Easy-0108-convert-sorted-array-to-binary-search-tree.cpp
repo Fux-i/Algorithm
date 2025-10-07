5429652172760fc23eab6b0d0b313f5c1ce358d4
fuxi <1538130391@qq.com>
2025-10-07 09:47:41 +0800

    2

---
+++ cpp/BinaryTree/Easy-0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	TreeNode* sortedArrayToBST(std::vector<int>& nums)
+	{
+		int l = 0;
+		int r = nums.size() - 1;
+		return rec(nums, l, r);
+	}
+
+	TreeNode* rec(std::vector<int>& nums, int l, int r)
+	{
+		if (l > r) return nullptr;
+		int mid = (l + r) / 2;
+		TreeNode* node = new TreeNode(nums[mid]);
+		node->left = rec(nums, l, mid-1);
+		node->right = rec(nums, mid+1, r);
+		return node;
+	}
+};
