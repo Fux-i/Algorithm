7e0ff3c61f48024db4c04ff1ee2cfa696e1af70a
fuxi <1538130391@qq.com>
2025-10-10 10:09:47 +0800

    2

---
+++ cpp/BachTracing/Mid-0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+﻿#include <vector>
+
+class Solution {
+	int sum = 0;
+	std::vector<int> one;
+	std::vector<std::vector<int>> ret;
+public:
+	std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
+        rec(candidates, target, 0);
+		return ret;
+	}
+
+	void rec(std::vector<int>& nums, int target, int idx)
+	{
+		for (int i = idx; i < nums.size(); i++)
+		{
+			int num = nums[i];
+			
+			one.push_back(num);
+			sum += num;
+			if (sum == target)
+				ret.push_back(one);
+			if (sum < target)
+				rec(nums, target, i);
+			
+			sum -= num;
+			one.pop_back();
+		}
+	}
+};
---
+++ cpp/BachTracing/Mid-0040-combination-sum-ii.cpp
@@ -0,0 +1,39 @@
+﻿#include <algorithm>
+#include <vector>
+
+class Solution {
+	int sum = 0;
+	std::vector<int> one;
+	std::vector<std::vector<int>> ret;
+public:
+	std::vector<std::vector<int>> combinationSum2(std::vector<int>& candidates, int target) {
+		std::sort(candidates.begin(), candidates.end());
+		rec(candidates, target, 0);
+		return ret;
+	}
+
+	void rec(std::vector<int>& nums, int target, int idx)
+	{
+		if (sum == target)
+		{
+			ret.push_back(one);
+			return;
+		}
+		
+		for (int i = idx; i < nums.size(); i++)
+		{
+			if (i > idx && nums[i] == nums[i-1]) continue;
+			if (sum + nums[i] > target) break;
+			
+			int num = nums[i];
+			
+			one.push_back(num);
+			sum += num;
+			
+			rec(nums, target, i);
+			
+			sum -= num;
+			one.pop_back();
+		}
+	}
+};
