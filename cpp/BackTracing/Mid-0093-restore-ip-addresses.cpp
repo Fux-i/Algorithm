2eb17e4521e959748f939e5854795c06159261b9
fuxi <1538130391@qq.com>
2025-10-12 11:31:53 +0800

    2

---
+++ cpp/BackTracing/Mid-0093-restore-ip-addresses.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+
+class Solution
+{
+public:
+	std::vector<std::string> restoreIpAddresses(std::string s)
+	{
+		std::vector<std::string> result;
+		std::string current;
+		backtrack(s, 0, 0, current, result);
+		return result;
+	}
+private:
+	void backtrack(const std::string& s, int start, int dots,
+	               std::string& current, std::vector<std::string>& result)
+	{
+		// 终止条件
+		if (dots == 3)
+		{
+			if (isValidSegment(s.substr(start)))
+				result.push_back(current + s.substr(start));
+			return;
+		}
+
+		// 尝试1-3个字符
+		for (int len = 1; len <= 3 && start + len < s.size(); ++len)
+		{
+			std::string segment = s.substr(start, len);
+			if (isValidSegment(segment))
+			{
+				std::string newCurrent = current + segment + ".";
+				backtrack(s, start + len, dots + 1, newCurrent, result);
+			}
+		}
+	}
+
+	bool isValidSegment(const std::string& segment)
+	{
+		if (segment.empty() || segment.size() > 3)
+			return false;
+		if (segment.size() > 1 && segment[0] == '0')
+			return false;
+		int num = std::stoi(segment);
+		return num >= 0 && num <= 255;
+	}
+};
--- cpp/BackTracing/Mid-0131-palindrome-partitioning.cpp
+++ cpp/BackTracing/Mid-0131-palindrome-partitioning.cpp
@@ -5,29 +5,28 @@ class Solution
 {
 	size_t size;
 	std::string str;
-	std::vector<std::vector<bool>> dp_;
+	std::vector<std::vector<bool>> dp;
 	std::vector<std::string> one;
 	std::vector<std::vector<std::string>> ret;
 public:
-	std::vector<std::vector<std::string>> partition(std::string s)
+	std::vector<std::vector<std::string>> partition(std::string& s)
 	{
 		size = s.size();
 		str = std::move(s);
-		std::vector<std::vector<bool>> dp(size, std::vector<bool>(size, false));
+		
+		dp.assign(size, std::vector<bool>(size, false));
 		for (size_t i = 0; i < size; i++)
 			dp[i][i] = true;
-		for (size_t len = 1; len < size; len++)
-			for (size_t i = 0; i + len < size; i++)	// 起始字符位置
+		for (int i = size - 1; i >= 0; i--)	// 起始字符位置
+			for (size_t len = 1; i + len < size; len++)
 			{
 				if (len == 1 || len == 2)
 					dp[i][i + len] = str[i] == str[i + len];
 				else
 					dp[i][i + len] = str[i] == str[i + len] && dp[i + 1][i + len - 1];
 			}
-		dp_ = std::move(dp);
-
+		
 		rec(0);
-
 		return ret;
 	}
 
@@ -41,7 +40,7 @@ public:
 
 		for (size_t k = idx; k < size; k++)
 		{
-			if (dp_[idx][k])
+			if (dp[idx][k])
 			{
 				one.push_back(str.substr(idx, k - idx + 1));
 				rec(k + 1);
