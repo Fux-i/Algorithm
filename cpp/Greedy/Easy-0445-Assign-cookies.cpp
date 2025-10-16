a4c7869409bf95f59e0be80c94362d82bd501a81
fuxi <1538130391@qq.com>
2025-10-16 10:09:24 +0800

    2

---
+++ cpp/Greedy/Easy-0445-Assign-cookies.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+	int findContentChildren(std::vector<int>& g, std::vector<int>& s)
+	{
+		std::sort(g.begin(), g.end(), [](int a, int b){ return a > b; });
+		std::sort(s.begin(), s.end(), [](int a, int b){ return a > b; });
+		int sizeG = g.size();
+		int sizeS = s.size();
+		int i = 0;
+		int j = 0;
+		for ( ;i < sizeG && j < sizeS; i++)
+		{
+			if (g[i] <= s[j]) j++;
+		}
+		return j;
+	}
+};
---
+++ cpp/Greedy/Mid-0376-wiggle-subsequence.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+
+class Solution
+{
+public:
+	int wiggleMaxLength(std::vector<int>& nums)
+	{
+		int size = nums.size();
+		int maxSubLength = 1;
+		int lastDiff;
+		for (int i = 1; i < size; i++)
+		{
+			int curDiff = nums[i] - nums[i - 1];
+			if (curDiff == 0) continue;	// 相等，删一个（跳过）
+				
+			if (maxSubLength == 1)	// 第一个，记录差值后跳过
+			{
+				lastDiff = curDiff;
+				maxSubLength++;
+				continue;
+			}
+			
+			if (lastDiff > 0 && curDiff > 0 || lastDiff < 0 && curDiff < 0)	// 两次差值同大
+				continue;
+
+			// 两次差值一大一小
+			lastDiff = curDiff;
+			maxSubLength++;
+		}
+		
+		return maxSubLength;
+	}
+};
