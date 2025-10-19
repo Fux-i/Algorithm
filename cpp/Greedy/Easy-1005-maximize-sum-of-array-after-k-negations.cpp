d6987f68c9164a9521c03d3d5f999590a3eef2ab
fuxi <1538130391@qq.com>
2025-10-19 14:49:36 +0800

    2

---
+++ cpp/Greedy/Easy-1005-maximize-sum-of-array-after-k-negations.cpp
@@ -0,0 +1,54 @@
+﻿#include <algorithm>
+#include <vector>
+
+class Solution
+{
+public:
+	int largestSumAfterKNegations(std::vector<int>& nums, int k)
+	{
+		std::sort(nums.begin(), nums.end());
+		
+		int sum = 0;
+		size_t  size = nums.size();
+		
+		for (size_t i = 0; i < size; i++)
+		{
+			int num = nums[i];
+			if (k == 0)
+			{
+				sum += num;
+				continue;
+			}
+			if (num < 0)
+			{
+				sum += -num;
+				k--;
+				continue;
+			}
+			if (i > 0 && nums[i-1] < 0)	// 首次遇到最小的非负数
+			{
+				if (-nums[i-1] < num)
+				{
+					if (k % 2 != 0) sum += 2 * nums[i-1];
+					sum += num;
+				}
+				else
+				{
+					if (k % 2 != 0) sum += -num;
+					else sum += num;
+				}
+				k = 0;
+			}
+			else
+			{
+				if (k % 2 != 0) sum += -num;
+				else sum += num;
+				k = 0;
+			}
+		}
+		if (k > 0 && k % 2 != 0)
+				sum += 2 * nums[size - 1];
+		
+		return sum;
+	}
+};
---
+++ cpp/Greedy/Mid-0134-gas-station.cpp
@@ -0,0 +1,50 @@
+﻿#include <vector>
+
+class Solution
+{
+public:
+	int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost)
+	{
+		int n = gas.size();
+		int start = 0;
+		while (start < n) {
+			int sumOfGas = 0, sumOfCost = 0;
+			int order = 0;
+			while (order < n) {
+				int index = (start + order) % n;
+				sumOfGas += gas[index];
+				sumOfCost += cost[index];
+				if (sumOfCost > sumOfGas) 
+					break;
+				order++;
+			}
+			if (order == n) 
+				return start;
+			 
+			start = start + order + 1;
+			
+		}
+		return -1;
+	}
+	
+	/*
+	{
+		int n = gas.size();
+		
+		for (int start = 0; start < n; start++)
+		{
+			int total = 0;
+			for (int order = 0; order < n; order++)
+			{
+				int index = (start + order) % n;
+				total += gas[index];
+				total -= cost[index];
+				if (total < 0) break;
+				if (order == n - 1) return start;
+			}
+		}
+		return -1;
+	}
+	*/
+	
+};
