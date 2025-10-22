ae2071bd364fb75738a4ac4e56b62c0dba44acd6
fuxi <1538130391@qq.com>
2025-10-22 10:25:11 +0800

    2

---
+++ cpp/Greedy/Mid-0435-non-overlapping-intervals.cpp
@@ -0,0 +1,23 @@
+﻿#include <algorithm>
+#include <vector>
+
+class Solution
+{
+public:
+	int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals)
+	{
+        std::sort(intervals.begin(), intervals.end(), [](std::vector<int>& a, std::vector<int>& b)
+        {
+	        return a[1] < b[1];
+        });
+
+		int pos = intervals[0][1];
+		int ans = 0;
+		for (int i = 1; i < intervals.size(); i++)
+		{
+			if (intervals[i][0] < pos) ans++;
+			else pos = intervals[i][1];
+		}
+		return ans;
+	}
+};
---
+++ cpp/Greedy/Mid-0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,29 @@
+﻿#include <algorithm>
+#include <vector>
+
+class Solution
+{
+public:
+	int findMinArrowShots(std::vector<std::vector<int>>& points)
+	{
+		std::sort(points.begin(), points.end(), [](std::vector<int>& a, std::vector<int>& b)
+		{
+			if (a[0] == b[0]) return a[1] < b[1];
+			return a[0] < b[0];
+		});
+		
+		int pos = points[0][1];
+		int ans = 1;
+
+		for (auto one : points)
+		{
+			if (one[0] > pos)
+			{
+				pos = one[1];
+				ans++;
+			}
+		}
+
+		return ans;
+	}
+};
