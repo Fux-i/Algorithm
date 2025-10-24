134852ff2a27daea2bec3db98d491e8f252a7adf
fuxi <1538130391@qq.com>
2025-10-24 11:00:18 +0800

    2

---
+++ cpp/Greedy/Hard-0968-binary-tree-cameras.cpp
@@ -0,0 +1,56 @@
+﻿struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+	int minCameraCover(TreeNode* root)
+	{
+		bool camera, covered;
+		int ans = dfs(root, camera, covered);
+		if (!covered)
+			ans++;
+		return ans;
+	}
+
+	int dfs(TreeNode* root, bool& camera, bool& covered)
+	{
+		if (!root)
+		{
+			camera = false;
+			covered = true;
+			return 0;
+		}
+
+		bool left_covered;
+		bool right_covered;
+		bool left_camera;
+		bool right_camera;
+		int num_left_camera = dfs(root->left, left_camera, left_covered);
+		int num_right_camera = dfs(root->right, right_camera, right_covered);
+
+
+		if (!left_covered || !right_covered)
+		{
+			camera = true;
+			covered = true;
+			return num_left_camera + num_right_camera + 1;
+		}
+		if (left_camera || right_camera)
+		{
+			camera = false;
+			covered = true;
+			return num_left_camera + num_right_camera;
+		}
+		camera = false;
+		covered = false;
+		return num_left_camera + num_right_camera;
+	}
+};
---
+++ cpp/Greedy/Mid-0738-monotone-increasing-digits.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+
+class Solution
+{
+public:
+	int monotoneIncreasingDigits(int n)
+	{
+		std::string strN = std::to_string(n);
+		int i = 1;
+		for (; i < strN.length() && strN[i - 1] <= strN[i]; i++);
+		
+		if (i < strN.length())
+		{
+			while (i > 0 && strN[i - 1] > strN[i])
+			{
+				strN[i - 1] -= 1;
+				i -= 1;
+			}
+			for (i += 1; i < strN.length(); ++i)
+				strN[i] = '9';
+		}
+		return std::stoi(strN);
+	}
+};
