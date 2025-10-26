a9adac5ee7d897cff774830373f7b66f1c454747
fuxi <1538130391@qq.com>
2025-10-26 10:15:35 +0800

    2

---
+++ cpp/DP/Mid-0096-unique-binary-search-trees.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+
+class Solution
+{
+public:
+	int numTrees(int n)
+	{
+		if (n == 1) return 1;
+		std::vector<int> dp(n + 1, 1);
+		for (int i = 2; i <= n; i++)
+		{
+			int num = 0;
+			for (int j = 0; j < i; j++)
+				num += dp[j] * dp[i - 1 - j];
+			dp[i] = num;
+		}
+		return dp[n];
+	}
+};
---
+++ cpp/DP/Mid-0343-integer-break.cpp
@@ -0,0 +1,27 @@
+﻿class Solution
+{
+public:
+	int integerBreak(int n)
+	{
+		if (n == 2) return 1;
+		if (n < 6) return (n - 2) * 2;
+		
+		int remain = n % 3, ans = 1;
+		if (remain == 0)
+			for (int i = 0; i < n / 3; i++)
+				ans *= 3;
+		else if (remain == 1)
+		{
+			for (int i = 0; i < n / 3 - 1; i++)
+				ans *= 3;
+			ans *= 4;
+		}
+		else
+		{
+			for (int i = 0; i < n / 3; i++)
+				ans *= 3;
+			ans *= 2;
+		}
+		return ans;
+	}
+};
