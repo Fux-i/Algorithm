121e7b56b3f433ccf36fcf830902ef81033a2913
fuxi <1538130391@qq.com>
2025-10-29 12:31:17 +0800

    2

---
+++ cpp/DP/Mid-0494-target-sum.cpp
@@ -0,0 +1,22 @@
+﻿#include <numeric>
+#include <vector>
+#include <bitset>
+
+class Solution
+{
+public:
+	int findTargetSumWays(std::vector<int>& nums, int target)
+	{
+		int s = std::accumulate(nums.begin(), nums.end(), 0) - abs(target);
+		if (s < 0 || s % 2)
+			return 0;
+
+		int m = s / 2; // 背包容量
+		std::vector<int> f(m + 1);
+		f[0] = 1;
+		for (int x : nums)
+			for (int c = m; c >= x; c--)
+				f[c] += f[c - x];
+		return f[m];
+	}
+};
---
+++ cpp/DP/Mid-1049-last-stone-weight-ii.cpp
@@ -0,0 +1,24 @@
+﻿#include <bitset>
+#include <numeric>
+#include <vector>
+
+class Solution
+{
+public:
+	int lastStoneWeightII(std::vector<int>& stones)
+	{
+		const int sum = std::accumulate(stones.begin(), stones.end(), 0);
+		const int halfSum = sum / 2;
+		
+		std::bitset<1501> set;
+		set[0] = true;
+		for (const auto stone : stones)
+			set |= set << stone;
+
+		int i = halfSum;
+		while (!set[i])
+			i--;
+
+        return sum - 2 * i;
+	}
+};
