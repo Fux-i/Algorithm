bd8fbfc8bcd98b6b96d2b5434f9e2901fe072e8f
fuxi <1538130391@qq.com>
2025-11-04 10:19:57 +0800

    2

---
+++ cpp/DP/Mid-0377-combination-sum-iv.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int combinationSum4(vector<int>& nums, int target) {
+		vector<size_t> dp(target + 1);
+		dp[0] = 1;
+		for (int i = 1; i <= target; i++)
+			for (int num : nums)
+				if (num <= i)
+					dp[i] += dp[i-num];
+
+		return dp[target];
+	}
+};
---
+++ cpp/DP/Mid-0518-coin-change-ii.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int change(int amount, vector<int>& coins)
+	{
+		if (amount == 0) return 1;
+		vector<size_t> dp(amount+1, 0);	// 测试用例有问题.. 用int会overflow
+		dp[0] = 1;
+		for (int coin : coins)
+			for (size_t j = coin; j <= amount; j++)
+				dp[j] = dp[j] + dp[j-coin];
+		return dp[amount];
+	}
+	/*
+	int change(int amount, vector<int>& coins)
+	{
+		if (amount == 0) return 1;
+		
+		size_t size = coins.size();
+		vector dp(size+1, vector<size_t>(amount+1, 0));
+		for (size_t i = 0; i < size; i++)
+		{
+			size_t coin = coins[i];
+			for (size_t j = 1; j <= amount; j++)
+			{
+				if (j == coin) dp[i+1][j] = dp[i][j] + 1;
+				else if (j < coin) dp[i+1][j] = dp[i][j];
+				else dp[i+1][j] = dp[i][j] + dp[i+1][j-coin];
+			}
+		}
+		return dp[size][amount];
+	}*/
+};
