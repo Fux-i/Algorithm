31f5fdfe6db4d7e8384806059ce5bf91a0d255a2
fuxi <1538130391@qq.com>
2025-11-05 09:34:08 +0800

    2

---
+++ cpp/DP/Mid-0279-perfect-squares.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int numSquares(int n)
+	{
+		vector<int> dp(n + 1, INT_MAX);
+		dp[0] = 0;
+		for (int j = 1; j <= n; j++)
+			for (int i = 1; i * i <= j; i++)
+				dp[j] = min(dp[j], dp[j - i * i] + 1);
+		return dp[n];
+	}
+};
