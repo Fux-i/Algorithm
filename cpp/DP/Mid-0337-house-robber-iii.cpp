df71ae2622d3d473905ae56859efcb2e958f06dc
fuxi <1538130391@qq.com>
2025-11-07 10:53:22 +0800

    1

---
+++ cpp/DP/Mid-0337-house-robber-iii.cpp
@@ -0,0 +1,37 @@
+﻿#include <algorithm>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+
+class Solution
+{
+public:
+	int rob(TreeNode* root)
+	{
+		auto dfsFunc = [&](this auto&& dfs, TreeNode* node) -> pair<int, int>
+		{
+			if (!node)
+				return {0, 0}; // {偷, 不偷}
+			
+			auto [leftStolen, leftNot] = dfs(node->left);
+			auto [rightStolen, rightNot] = dfs(node->right);
+
+			int stolen = node->val + leftNot + rightNot;
+			int notStolen = max(leftStolen, leftNot) + max(rightStolen, rightNot);
+
+			return {stolen, notStolen};
+		};
+
+		auto [stolen, notStolen] = dfsFunc(root);
+		return max(stolen, notStolen);
+	}
+};
