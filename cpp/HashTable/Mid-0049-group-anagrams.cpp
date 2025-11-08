0d44da66283203ae4a3a16640be1dca8a8fa7788
fuxi <1538130391@qq.com>
2025-11-08 11:29:22 +0800

    1

---
+++ cpp/HashTable/Mid-0049-group-anagrams.cpp
@@ -0,0 +1,24 @@
+﻿#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs)
+	{
+		unordered_map<string, vector<string>> mp;
+		for (string& str : strs)
+		{
+			string key = str;
+			ranges::sort(key);
+			mp[key].emplace_back(str);
+		}
+		vector<vector<string>> ans;
+		for (auto it = mp.begin(); it != mp.end(); ++it)
+			ans.emplace_back(it->second);
+		return ans;
+	}
+};
