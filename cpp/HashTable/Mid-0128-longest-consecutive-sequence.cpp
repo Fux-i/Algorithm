528a38b77fa2ffc14d0410cffa1bf37ae0b32fa1
fuxi <1538130391@qq.com>
2025-11-12 16:27:02 +0800

    3

---
+++ cpp/HashTable/Mid-0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,25 @@
+﻿#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	int longestConsecutive(vector<int>& nums)
+	{
+		unordered_set<int> set(nums.begin(), nums.end());
+		
+		int maxLen = 0;
+		for (int num : set)
+		{
+			if (set.contains(num - 1)) continue;
+			int len = 1;
+			while (set.contains(++num))
+				len++;
+			maxLen = max(maxLen, len);
+		}
+		return maxLen;
+	}
+};
