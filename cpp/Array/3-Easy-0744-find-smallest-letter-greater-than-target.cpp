d544130d5c427062debba105b6f887a696fcadc0
fuxi <1538130391@qq.com>
2026-01-31 18:35:38 +0800

    1

---
+++ cpp/Array/3-Easy-0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	char nextGreatestLetter(vector<char> &letters, char target) {
+		for (char letter : letters) {
+			if (letter > target) {
+				return letter;
+			}
+		}
+		return letters[0];
+	}
+};
